b8c7203f540c475f74439de9576077e248727365
srinathkrishnan1998 <31008815+srinathkrishnan1998@users.noreply.github.com>
2017-08-15 19:42:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('.articleone',function(req,res){
+app.get('articleone',function(req,res){
    res.sendfile('article 0ne will be served soon'); 
 });
 
