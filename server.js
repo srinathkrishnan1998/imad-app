4bf10280ca7a0ef8635076d8c5ad553dcb375021
srinathkrishnan1998 <31008815+srinathkrishnan1998@users.noreply.github.com>
2017-08-15 19:54:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleone',function(req,res){
-   res.send('article 0ne will be served soon'); 
+     res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
+   
 });
 
 app.get('/ui/style.css', function (req, res) {
